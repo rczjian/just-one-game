30db62f639826e819cab2a2c3f672aa956263076
Raymond <raymond.chan@u.nus.edu>
2022-02-06 17:49:31 +0800

    Use wss for prod

--- just-one-game-frontend/src/components/hooks/useWebSocket.js
+++ just-one-game-frontend/src/components/hooks/useWebSocket.js
@@ -2,10 +2,11 @@ import React from "react";
 
 export default function useWebSocket({ setConnection, handleMessage }) {
   const ws = React.useRef(null);
+  const wsProtocol = process.env.REACT_APP_SECURE_WS ? "wss://" : "ws://";
   const host = process.env.REACT_APP_BACKEND_HOST || "192.168.1.75:8080";
 
   React.useEffect(() => {
-    ws.current = new WebSocket(`ws://${host}`);
+    ws.current = new WebSocket(`${wsProtocol}${host}`);
     ws.current.onopen = () => console.log("ws opened");
     ws.current.onclose = () => {
       console.log("ws closed");
